659daf204ed08cfc30887474642a7295138243a8
Paul Hondl <paul.hondl@yahoo.de>
2023-07-30 11:10:10 +0200

    added logic for swapping big image

--- src/pages/AccomodationDetails/AccomodationDetails.jsx
+++ src/pages/AccomodationDetails/AccomodationDetails.jsx
@@ -15,46 +15,57 @@ function AccomodationDetails() {
   const {accomodation_id} = useParams();
 
   const [accomodation, setAccomodation] = useState({});
+  const [images, setImages] = useState([])
+
 
   useEffect(() => {
     fetch(`https://unilife-server.herokuapp.com/properties/${accomodation_id}`)
       .then(response => response.json())
-      .then(data => setAccomodation(data))
+      .then(data => {
+        setAccomodation(data)
+        setImages(data.images)
+      })
       .catch(error => console.log(error))
     }, [])
 
-  useEffect(() => {
-    console.log(...Object.entries(accomodation.bedroom_prices))
-  }, [accomodation])
+    useEffect(() => {
+      console.log(images)
+    }, [images])
 
-  /* 
-    property_description (string)
-    key_features (array)
-    bedroom_prices: {bedroom_one: 160, bedroom_two: 150,... }
-  */
 
-  return (
+    function swapImages(newBigImage) {
+      console.log(images.indexOf(newBigImage))
+      const swappedImages = [...images]
+      const formerBigImage = swappedImages[0];
+      swappedImages[0] = newBigImage;
+      swappedImages[images.indexOf(newBigImage)] = formerBigImage;
+      console.log(swappedImages)
+      setImages(swappedImages)
+    }
+
+
+ return (
       <div className="details-container">
         <div className="back-link">
         <Link to=".." relative="path"><span className="left-circle"><FiArrowLeftCircle className="left-circle-icon" /></span> <p>Back to search</p></Link>
       </div>
         <div className="details-top">
-          <div className="details-top-left">
+         {images?.length && <div className="details-top-left">
             <div className="big-picture">
-              <img src={accomodation?.images[0]} alt="some pic" />
+              <img src={images[0]} alt="some pic" />
             </div>
             <div className="small-pictures">
               <div className="small-picture">
-                <img src={accomodation.images[1]} alt="some pic" />
+                <img src={images[1]} alt="some pic" onClick={(e) => swapImages(e.target.src)} />
               </div>
               <div className="small-picture">
-                <img src={accomodation.images[2]} alt="some pic" />
+                <img src={images[2]} alt="some pic" onClick={(e) => swapImages(e.target.src)} />
               </div>
               <div className="small-picture">
-              <img src={accomodation.images[3]} alt="some pic" />
+                <img src={images[3]} alt="some pic" onClick={(e) => swapImages(e.target.src)} />
               </div>
             </div>
-          </div>
+          </div>}
           <div className="details-top-right">
             <div className="accomodation-information">
               <h2>
@@ -65,11 +76,11 @@ function AccomodationDetails() {
               <div className="accomodation-specs">
                 <div className="spec">
                   <h4>Bedrooms</h4>
-                  <p><img src="../../../public/bed_FILL0_wght300_GRAD0_opsz48_blue.png" alt="bedrooms" /> {accomodation.bedroom_count}</p>
+                  <p><img src="bed_FILL0_wght300_GRAD0_opsz48_blue.png" alt="bedrooms" /> {accomodation.bedroom_count}</p>
                 </div>
                 <div className="spec">
                   <h4>Bathrooms</h4>
-                  <p><img src="../../../public/bathtub_FILL0_wght300_GRAD0_opsz48_blue.png" alt="bathtub" />{accomodation.bathroom_count}</p>
+                  <p><img src="bathtub_FILL0_wght300_GRAD0_opsz48_blue.png" alt="bathtub" />{accomodation.bathroom_count}</p>
                 </div>
                 <div className="spec">
                   <h4>Property Type</h4>
